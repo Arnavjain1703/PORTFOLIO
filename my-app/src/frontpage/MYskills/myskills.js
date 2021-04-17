fd25cbd13c99093315a1f7fde0121f72e733954e
Arnavjain1703 <arnavjainkht@gmail.com>
2021-04-18 00:04:45 +0530

    my skills

--- my-app/src/frontpage/MYskills/myskills.js
+++ my-app/src/frontpage/MYskills/myskills.js
@@ -5,7 +5,40 @@ class Myskill extends Component{
    {
        return (
          <div className="myskills">
-
+             <div className="cover row"><div className="col-sm-6 cover2"></div><div className="col-sm-6 cover2"></div></div> 
+                <div className="heading"><div className="content">My Skills 📖</div></div>
+                <div className="row padding display">
+                  <div className="col-sm-4 center"><img src="/assets/Angular.svg"></img></div>
+                  <div className="col-sm-4 center"><img src="/assets/react.svg"></img></div>
+                  <div className="col-sm-4 center"><img  src="/assets/node.svg"></img></div>
+                </div>
+                <div className="row padding display">
+                  <div className="col-sm-4 center"><img src="/assets/c.svg"></img></div>
+                  <div className="col-sm-4 center"><img src="/assets/mongo.svg"></img></div>
+                  <div className="col-sm-4 center"><img src="/assets/git.svg"></img></div>
+                </div>
+                <div className="row padding display">
+                  <div className="col-sm-4 center"><img src="/assets/html5.svg"></img></div>
+                  <div className="col-sm-4 center"><img src="/assets/css3.svg"></img></div>
+                  <div className="col-sm-4 center"><img src="/assets/js.svg"></img></div>
+                </div>
+                <div className="display2">
+                  <div  style={{textAlign:"center",width:"33vw"}}><img src="/assets/Angular.svg"></img></div>
+                  <div  style={{textAlign:"center",width:"33vw"}}><img src="/assets/react.svg"></img></div>
+                  <div  style={{textAlign:"center",width:"33vw"}}><img src="/assets/node.svg"></img></div>
+                  </div>
+                  <div className="display2">
+                  <div  style={{textAlign:"center",width:"33vw"}}><img src="/assets/c.svg"></img></div>
+                  <div  style={{textAlign:"center",width:"33vw"}}><img src="/assets/mongo.svg"></img></div>
+                  <div style={{textAlign:"center",width:"33vw"}}><img src="/assets/git.svg"></img></div>
+                  </div>
+                  <div className="display2">
+                  <div  style={{textAlign:"center",width:"33vw"}}><img src="/assets/html5.svg"></img></div>
+                  <div  style={{textAlign:"center",width:"33vw"}}><img src="/assets/css3.svg"></img></div>
+                  <div  style={{textAlign:"center",width:"33vw"}}><img src="/assets/js.svg"></img></div>
+                </div>
+               
+           
          </div>       
        )
    }
